51c16637958b377507f39097b37f2942f0cb8264
jinsoo <89149734+Jinsoo1004@users.noreply.github.com>
2023-04-11 16:09:44 +0900

    :hammer: :: 로그인 조건 추가
    로그인 조건과 버그수정

--- src/components/Auth/Login/index.jsx
+++ src/components/Auth/Login/index.jsx
@@ -41,7 +41,20 @@ const Login = () => {
         setPassword(event.target.value);
     }
 
+    const onKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            handleLogin();
+        }
+    }
+
     const handleLogin = async () => {
+        if (id === '' || password === '') {
+            Toast.fire({
+                icon: 'error',
+                title: '아이디와 비밀번호를 입력해주세요 !'
+            })
+            return;
+        }
         const data = { id, password };
         try {
             // 로그인 성공
@@ -49,10 +62,7 @@ const Login = () => {
             const { accessToken, refreshToken } = response.data;
             localStorage.setItem('accessToken', accessToken);
             document.cookie = `refreshToken=${refreshToken}`;
-            Toast.fire({
-                icon: 'success',
-                title: '로그인 성공 !'
-            })
+
             navigate("/");
         } catch (error) {
             // 로그인 실패 
@@ -60,21 +70,21 @@ const Login = () => {
                 icon: 'error',
                 title: '로그인 실패 !'
             })
-            console.log(error);
+            // console.log(error);
         }
     }
 
     return (
         <div>
             <Flex>
-                <form className='form'>
+                <form className='form' onKeyPress={onKeyPress}>
                     <div className='Ang-Text'>로그인</div>
                     <Reservation />
-                    <input className='input' type='text' id='input-id' value={id} onChange={handleIdChange}></input>
-                    <input className='input' type='password' id='input-pw' value={password} onChange={handlePasswordChange}></input>
+                    <input className='input' type='text' id='input-id' placeholder='UserName' value={id} onChange={handleIdChange}></input>
+                    <input className='input' type='password' id='input-pw' placeholder='Password' value={password} onChange={handlePasswordChange}></input>
                     <button className='button' type='button' onClick={handleLogin}>로그인</button>
                     <div className='Goto-Signin-Text'>아직 회원이 아니신가요?&nbsp;
-                        <span className='Goto-Signin-Href'>회원가입</span>
+                        <span className='Goto-Signin-Href' onClick={() => { navigate('/signup') }}>회원가입</span>
                     </div>
                 </form>
             </Flex>
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,14 +1,28 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2'
 
 const Home = () => {
     const navigate = useNavigate();
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 2500,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
     useEffect(() => {
         // 로컬 스토리지에서 accessToken 가져오기
         const accessToken = localStorage.getItem('accessToken');
         // axios를 사용하여 POST 요청 보내기
-        axios.post('http://10.80.161.45:8080/api/user/profile', null,{
+        axios.post('http://10.80.161.45:8080/api/user/profile', null, {
             headers: {
                 // mode: 'cors',
                 'Authorization': `Bearer ${accessToken}`,
@@ -16,8 +30,24 @@ const Home = () => {
         })
             .then(response => {
                 // 요청이 성공적으로 처리됨
-                // console.log(response);
-                console.log("로그인 성공");
+                // console.log(response.data.userType);
+                const userType = response.data.userType;
+                if (userType === 'T') {
+                    console.log("로그인 성공");
+                    Toast.fire({
+                        icon: 'success',
+                        title: '로그인 성공 !'
+                    })
+                }
+                else {
+                    console.log("유효하지 않은 계정");
+                    Toast.fire({
+                        icon: 'error',
+                        title: '유효하지 않은 게정입니다 !'
+                    })
+                    navigate("/login");
+                }
+
             })
             .catch(error => {
                 // 요청이 실패함
@@ -26,7 +56,7 @@ const Home = () => {
                 navigate("/login");
 
             });
-    }, ); // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때만 실행
+    }, [navigate, Toast]); // 빈 배열을 전달하여 컴포넌트가 처음 렌더링될 때만 실행
     return (
         <div>
             <h1>Home Page</h1>
