9788fc094e07c1abd545e86f02101097109cbb3d
jinsoo <jinsoo1004@proton.me>
2023-05-10 23:36:50 +0900

    🔨 :: 헤헤
    5/10

--- src/hooks/CheckLogin.jsx
+++ src/hooks/CheckLogin.jsx
@@ -1,4 +1,44 @@
+import axios from "axios";
 import api from "./Api";
+
+// 쿠키를 가져오는 함수
+function getCookie(name) {
+    const nameEQ = `${name}=`;
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === " ") {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) === 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+const refreshAccessToken  = async () => {
+    const RefreshURL = `${URL}/api/account/refresh.do`;
+    const refreshToken = getCookie("refreshToken");
+    try {
+        const response = await axios.post(
+            RefreshURL,
+            {},
+            {
+                headers: {
+                    Authorization: `Bearer ${refreshToken}`,
+                },
+            }
+        );
+        const GetaccessToken = response.data.data.accessToken;
+        const GetrefreshToken = response.data.data.refreshToken;
+        localStorage.setItem("accessToken", GetaccessToken);
+        document.cookie = `refreshToken=${GetrefreshToken}`;
+        return GetaccessToken;
+    } catch (error) {
+        return null;
+    }
+}
 
 export const CheckLogin = async (accessToken) => {
     try {
@@ -13,6 +53,7 @@ export const CheckLogin = async (accessToken) => {
             return userType === "T";
         } else if (userData.status === 401) {
             //작업수행
+            refreshAccessToken(); //TODO
         }
         else {
             console.log("서버 에러");
