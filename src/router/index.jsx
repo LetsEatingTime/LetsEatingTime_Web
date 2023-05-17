b9306b799757d7e3794f68140728d53b305845b7
jinsoo <jinsoo1004@proton.me>
2023-05-17 14:45:21 +0900

    Add :: 유저 관리
    2023/05/17 - 유저 관리 페이지를 개발하였습니다.
    유저 관리 페이지에선 유저의 계정을 관리 할 수 있습니다

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -5,12 +5,14 @@ import Loading from "../components/Loading";
 import UserDetail from "../components/UserDetail";
 import UserApply from "../components/Auth/UserApply";
 import NavBar from "../components/NavBar";
+import ManageUser from "../components/Auth/ManageUser";
 
 const Login = lazy(() => import("../components/Auth/Login"));
 const SignUp = lazy(() => import("../components/Auth/SignUp"));
 const Home = lazy(() => import("../components/Home"));
 const NotFound = lazy(() => import("../components/NotFound"));
 const Users = lazy(() => import("../components/Users"));
+
 
 const Router = () => {
     return (
@@ -21,7 +23,8 @@ const Router = () => {
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/users" element={<Users />} />
                 <Route path="/user" element={<UserDetail />} />
-                <Route path="/admin" element={<UserApply />} />
+                <Route path="/admin/apply" element={<UserApply />} />
+                <Route path="/admin/users" element={<ManageUser />} />
                 <Route path="/test" element={<NavBar />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
